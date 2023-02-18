6325c15cadae7df979cbd24f24b7d8c3f05e0406
Taylor Monacelli <taylormonacelli@gmail.com>
2023-02-17 16:26:19 -0800

    Add daily durations as sum of daily task durations

--- src/foundationlive/model.py
+++ src/foundationlive/model.py
@@ -18,11 +18,17 @@ class DailyEntry(pydantic.BaseModel):
     tasks: TaskList
     date: datetime.datetime
     invoice: int
+    total_time_sec: typing.Optional[int] = None
 
     @pydantic.validator("date", pre=True)
     def parse_date_as_datetime_obj(cls, v):
         dt = dateutil.parser.parse(v)
         return dt
+
+    @pydantic.validator("total_time_sec")
+    def prevent_none(cls, v):
+        assert v is not None, "total_time_sec may not be None"
+        return v
 
 
 class Timesheet(pydantic.BaseModel):
