ae91deab8039190e70736c0299f4451d82d270a9
Taylor Monacelli <taylormonacelli@gmail.com>
2023-02-27 07:18:20 -0800

    Remove unused AllinOneWriter

--- src/foundationlive/writer.py
+++ src/foundationlive/writer.py
@@ -20,25 +20,6 @@ class FileWriter(Writer):
         path.chmod(path.stat().st_mode | stat.S_IEXEC)
 
 
-@dataclasses.dataclass
-class AllInOneWriter(Writer):
-    file: str
-    path: pathlib.Path = None
-
-    def __post_init__(self):
-        self.path = pathlib.Path(self.file)
-
-    def zero(self):
-        open(self.path, "w").close()
-        return self
-
-    def write(self, output: str):
-        with open(self.path, "a") as f:
-            f.write(f"{output}\n")
-            f.write("-" * 20)
-            f.write("\n")
-
-
 class ConsoleWriter(Writer):
     def write(self, output: str):
         print(output)
